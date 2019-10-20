4b724849c8287bac5079003febda9be74b1cf850
Juliya-Potapchuk <potapchuk.julya@list.ru>
2019-10-20 23:35:46 +0300

    feat: add js and style for swiper

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ let headEdu = document.querySelector('.head-list');
 headEdu.addEventListener('click', function () {
     list.classList.toggle('list-education-open');
 });
+
+              // SWIPER
+var swiper = new Swiper('.swiper-container', {
+    cssMode: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+});
